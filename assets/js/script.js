15865937fac3aa69c5817abef34d4e208c620d40
tomas <tomashmz@yahoo.com>
2024-08-04 21:57:46 -0400

    Mejoras en la estructura

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function showContacts() {
+    document.querySelector("#contactsList").style.display="block"
+}
+
+function read() {
+    document.querySelector(".contacts-profile__name--pending").style.fontWeight="normal"
+}
